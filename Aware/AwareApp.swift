904223db3ceff588cdf599e3e2c71e3a8eb3286e
shamam <kafri.shamam@gmail.com>
2025-05-24 09:54:12 +0300

    Initial Commit

---
+++ Aware/AwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwareApp.swift
+//  Aware
+//
+//  Created by Shamam Alkafri on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwareTests/AwareTests.swift
@@ -0,0 +1,17 @@
+//
+//  AwareTests.swift
+//  AwareTests
+//
+//  Created by Shamam Alkafri on 24/05/2025.
+//
+
+import Testing
+@testable import Aware
+
+struct AwareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
